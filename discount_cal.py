495ffad3919501af2e73dc391e406db8ea45cbb5
ABIR H. KHANDOKAR <116947694+AKH570@users.noreply.github.com>
2022-11-04 19:01:34 +0600

    Add files via upload

--- discount_cal.py
+++ discount_cal.py
@@ -9,64 +9,92 @@ root.title('Daily Sales Accounting')
 
 head_lbl = Label(root,text='Company Discount Sales Calculations',font=('Microsoft Himalaya',24,'bold','italic'),fg='#FF00BF')
 head_lbl.place(relx=0.5,rely=0.03,anchor='center')
-
-
-
-def myCalculation():
-    WideDisAmt = float(OrgPrice.get())*0.1
-    v1=round(WideDisAmt,2)
-    WidDisc.set(str(v1))
-###Over$2000 Discount amount
-
-    if float(OrgPrice.get())> 2000:
-        over2000DiscountAmt = float(OrgPrice.get())*0.1
-        v2=round(over2000DiscountAmt,2)
-        Ovr2000.set(str(v2))
-    else:
-        over2000DiscountAmt = 0
-        Ovr2000.set( str( over2000DiscountAmt ) )
-
-### Total Discount Amount
-    TotalDiscount = WideDisAmt + over2000DiscountAmt
-    v3 =round(TotalDiscount,2)
-    totalDisAmt.set(str(v3))
-### Discounted Sale Price
-    discountSalePrice = float(OrgPrice.get())-TotalDiscount
-    v4 = round(discountSalePrice,2)
-    DiscPrice.set(str(v4))
-
-def submit():
-    if sales_manID_entry.get()=='' or  orginal_sale_entr.get() == '' :
-        messagebox.showerror( 'Error', 'Invalid Data' )
-    else:
-
-        try:
-            conn= sqlite3.connect('abcompany.db')
-            curr = conn.cursor()
-            curr.execute(""" CREATE TABLE IF NOT EXISTS salesinfo(
-                prod_id TEXT PRIMARY NOT NULL,
+'''
+try:
+    conn= sqlite3.connect('abcompany.db')
+    curr = conn.cursor()
+    curr.execute(""" CREATE TABLE IF NOT EXISTS salesinfo(
+                prod_id TEXT PRIMARY KEY,
                 emp_id TEXT NOT NULL,
                 orgi_price TEXT NOT NULL,
-                wide_disc TEXT NOT NULL,
+                wide_disc TEXT,
                 over2000_amt TEXT NOT NULL,
                 total_amt TEXT NOT NULL,
-                disc_price TEXT NOT NULL 
+                disc_price TEXT NOT NULL
                 )""")
-            curr.execute( "INSERT INTO salesinfo VALUES(:prod_id,:emp_id,:orgi_price,:wide_disc,:over2000_amt,:total_amt,:disc_price)",
-                             {'prod_id': prod_code_entry.get(),
-                              'emp_id': employe_id_entry.get(),
-                              'orgi_price': orginal_sale_entr.get(),
-                              'over2000_amt': Over200_Entry.get(),
-                              'total_amt': TotalDis_Entry.get(),
-                              'disc_price': DiscountPrice_Entry.get()
-                              } )
-            messagebox.showinfo('Success','All data added successfully')
+    conn.commit()
+except Exception as e:
+    print(e)
+'''
+def check():
+    if employe_id_entry.get()=='' or  orginal_sale_entr.get() == '' or prod_code_entry.get()=='':
+        messagebox.showerror( 'Error', 'Required field is empty' )
+    else:
+        #curr.execute('SHOW DATABASES')
+        #print(curr)
+
+        WideDisAmt = float(OrgPrice.get())*0.1
+        v1=round(WideDisAmt,2)
+        WidDisc.set(str(v1))
+###Over$2000 Discount amount
+        if float(OrgPrice.get())> 2000:
+            over2000DiscountAmt = float(OrgPrice.get())*0.1
+            v2=round(over2000DiscountAmt,2)
+            Ovr2000.set(str(v2))
+        else:
+            over2000DiscountAmt = 0
+            Ovr2000.set( str( over2000DiscountAmt ) )
+
+### Total Discount Amount
+        TotalDiscount = WideDisAmt + over2000DiscountAmt
+        v3 =round(TotalDiscount,2)
+        totalDisAmt.set(str(v3))
+### Discounted Sale Price
+        discountSalePrice = float(OrgPrice.get())-TotalDiscount
+        v4 = round(discountSalePrice,2)
+        DiscPrice.set(str(v4))
+
+def submit():
+            conn = sqlite3.connect( 'abcompany.db' )
+            curr = conn.cursor()
+            curr.execute( """ CREATE TABLE IF NOT EXISTS salesinfo(
+                            prod_id TEXT PRIMARY KEY,
+                            emp_id TEXT NOT NULL,
+                            orgi_price TEXT NOT NULL,
+                            wide_disc TEXT,
+                            over2000_amt TEXT NOT NULL,
+                            total_amt TEXT NOT NULL,
+                            disc_price TEXT NOT NULL
+                            )""" )
+            curr.execute("INSERT INTO salesinfo VALUES (:prod_id,:emp_id,:orgi_price,:wide_disc,:over2000_amt,:total_amt,:disc_price)",
+                {
+                    'prod_id': prod_code_entry.get(),
+                    'emp_id': employe_id_entry.get(),
+                    'orgi_price': orginal_sale_entr.get(),
+                    'wide_disc': wideDisc_Entry.get(),
+                    'over2000_amt': Over200_Entry.get(),
+                    'total_amt': TotalDis_Entry.get(),
+                    'disc_price': DiscountPrice_Entry.get()
+                } )
             conn.commit()
             conn.close()
-        except:
-            messagebox.showerror( 'Error', 'Something went wrong' )
 
-        messagebox.showinfo('','Good job')
+            employe_id_entry.delete( 0, END )
+            orginal_sale_entr.delete( 0, END )
+            prod_code_entry.delete( 0, END )
+            wideDisc_Entry.delete( 0, END )
+            Over200_Entry.delete( 0, END )
+            TotalDis_Entry.delete( 0, END )
+            DiscountPrice_Entry.delete( 0, END )
+            messagebox.showinfo( '', 'All record are added successfully' )
+
+def search():
+    conn = sqlite3.connect( 'abcompany.db' )
+    curr = conn.cursor()
+    curr.execute("SELECT * FROM salesinfo WHERE emp_id =? ",[search_entry.get()])
+    records=curr.fetchmany()
+    print(records)
+
 def idelete():
         pass
 
@@ -102,7 +130,7 @@ prod_lbl.place(x=400,y=200)
 prod_code_entry = Entry(root,bg='light grey',bd=3,fg='blue',font=('Cambria',16))
 prod_code_entry.place(x=550,y=200,width=100,height=30)
 
-search_btn =Button(root,text='Search', font=('Cambria',16),fg='black',bd=3)
+search_btn =Button(root,text='Search', font=('Cambria',16),fg='black',bd=3,command=search)
 search_btn.place(x=500,y=150,width=100,height=30)
 search_entry =Entry(root,bg='light grey',bd=3,fg='blue',font=('Cambria',16))
 search_entry.place(x=400,y=150,width=100,height=30)
@@ -114,7 +142,7 @@ orginal_sale_entr.place(x=220,y=200,width=150,height=30)
 
 Refresh_btn =Button(root,text='Refresh',font=('Cambria',18),fg='blue',command=refresh)
 Refresh_btn.place(x=320,y=250,width=80,height=35)
-OrgSal_btn =Button(root,text='Check',font=('Cambria',18),fg='blue',command=myCalculation)
+OrgSal_btn =Button(root,text='Check',font=('Cambria',18),fg='blue',command=check)
 OrgSal_btn.place(x=410,y=250,width=80,height=35)
 delete_btn =Button(root,text='Delete',font=('Cambria',18),fg='blue',command=idelete)
 delete_btn.place(x=500,y=250,width=80,height=35)
